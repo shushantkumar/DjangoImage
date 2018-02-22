5bf8bfc84a15af787afacca8408d74dcf2a1c24f
shushantkumar <shushantkmr2@gmail.com>
2018-02-22 18:10:05 +0530

    image API done

--- restimage/restimage/settings.py
+++ restimage/restimage/settings.py
@@ -128,3 +128,6 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_cdn")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"media_cdn")
+
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
